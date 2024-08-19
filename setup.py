3622ff262e3f6a909bafc391251950cb41ae9d8f
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-08-19 18:39:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 from setuptools import setup
 
-requirements = ['requests', "asyncio"]
+requirements = ['requests', 'asyncio', 'aiohttp']
 readme = ''
-with open("README.md", encoding="utf-8") as f:
+with open('README.md', encoding='utf-8') as f:
     readme = f.read()
+
 setup(
     name='stackhost',
-    author='iShiKki',
+    author='iShikki',
     author_email='ishikkiakabane@gmail.com',
     version='0.0.1',
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/ishikki-akabane/stackhost',
     license='GNU General Public License v3.0',
     classifiers=[
-        "Framework :: AsyncIO",
+        'Framework :: AsyncIO',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
+        'License :: OSI Approved :: GNU General Public License v3.0 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.7',
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Build Tools",
-
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Build Tools',
     ],
-    description='',
+    description='A shared hosting platform for students and developers offering affordable and free plans for testing and deploying projects.',
     include_package_data=True,
-    keywords=['server', 'hosting', 'docker', 'kubernet', 'database', 'free', 'code'],
-    install_requires=requirements
+    keywords=['server', 'docker', 'kubernet', 'database', 'free', 'code', 'hosting', 'development'],
+    install_requires=requirements,
 )
